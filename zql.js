8a2b2e762438d8e351ae3ab497b5e23f9fd7ab93
Ayah Alrifai <48604304+AyahAlrifai@users.noreply.github.com>
2021-11-03 15:17:22 +0200

    Update zql.js

--- zql.js
+++ zql.js
@@ -82,7 +82,7 @@ document.body.removeChild(input);
 document.getElementById("alert").innerHTML=`
   <div class="alert alert-success alert-dismissible fade show">
     <button type="button" class="close" data-dismiss="alert">&times;</button>
-    <strong>Success!</strong> This alert box could indicate a successful or positive action.
+    <strong>Success!</strong> Copy successful
   </div>`;
 window.setTimeout(()=>{
   document.getElementById("alert").innerHTML="";
